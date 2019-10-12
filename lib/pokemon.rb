9180998aa3db4cd705badc1754e883ef19c0dea1
Dean L Betty <deanlloyd64@gmail.com>
2019-10-12 02:54:04 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
+require "pry"
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+  
+  def initialize(id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?);
+    SQL
+    #binding.pry
+    db[:conn].execute(sql, self.name, self.type)
+    @id = db[:conn].execute("SELECT last_insert_rowid() FROM pokemon")
+  end
 end
